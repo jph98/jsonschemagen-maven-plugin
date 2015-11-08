040f42b0e625b4e3146e17a7cac9dc7f4547489d
Jonathan Holloway <jon.holloway@brightpearl.com>
2015-11-08 16:43:28 +0000

    Fix classpath dependencies, pretty print JSON

--- README.md
+++ README.md
@@ -4,27 +4,25 @@ Maven Plugin that generates JSON Schemas from Java objects using JJSchema.
 
 To use this add the following to your project with the package name to scan:
 
-    <plugin>
-        <groupId>com.froyo</groupId>
-        <artifactId>jsonschemagen-maven-plugin</artifactId>
-        <version>1.0</version>
-
-        <configuration>
-            <packageNameToScan>com.froyo</packageNameToScan>
-            <skipErrorNoDescriptorsFound>true</skipErrorNoDescriptorsFound>
-        </configuration>
-
-        <executions>
-            <execution>
-                <phase>validate</phase>
-                <goals>
-                    <goal>jsonschemagen</goal>
-                </goals>
-            </execution>
-        </executions>
-    </plugin>
+   <plugin>
+       <groupId>com.froyo</groupId>
+       <artifactId>jsonschemagen-maven-plugin</artifactId>
+       <version>1.0</version>
+       <executions>
+           <execution>
+               <phase>compile</phase>
+               <goals>
+                   <goal>jsonschemagen</goal>
+               </goals>
+           </execution>
+       </executions>
+       <configuration>
+           <packageNameToScan>com.froyo</packageNameToScan>
+           <schemaOutputDirectory>jsonschemas</schemaOutputDirectory>
+       </configuration>
+   </plugin>
 
 You should annotate each of your classes with the JJSchema Attributes annotation, for more info
 on this see:
 
-https://github.com/reinert/JJSchema
+    https://github.com/reinert/JJSchema
--- src/main/java/com/froyo/JsonSchemaGen.java
+++ src/main/java/com/froyo/JsonSchemaGen.java
@@ -1,42 +1,138 @@
 package com.froyo;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import com.cedarsoftware.util.io.JsonWriter;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.github.reinert.jjschema.Attributes;
 import com.github.reinert.jjschema.JsonSchemaGenerator;
 import com.github.reinert.jjschema.SchemaGeneratorBuilder;
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import org.apache.maven.artifact.DependencyResolutionRequiredException;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.descriptor.PluginDescriptor;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.classworlds.realm.ClassRealm;
 import org.reflections.Reflections;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.List;
 import java.util.Set;
 
 /**
- * https://maven.apache.org/guides/plugin/guide-java-plugin-development.html
+ * See: https://maven.apache.org/guides/plugin/guide-java-plugin-development.html
+ * See: http://stackoverflow.com/questions/2659048/add-maven-build-classpath-to-plugin-execution-classpath
  */
 @Mojo(name = "jsonschemagen")
 public class JsonSchemaGen extends AbstractMojo {
 
+    private static final boolean DEBUG = false;
+
     @Parameter
     private String packageNameToScan;
 
+    @Parameter
+    private String schemaOutputDirectory;
+
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    private MavenProject project;
+
+    /**
+     * @parameter default-value="${descriptor}"
+     */
+    @Component
+    private PluginDescriptor descriptor;
+
     public void execute() throws MojoExecutionException {
 
-        getLog().info("Execute JSON Schema Generator for package name: " + packageNameToScan);
+        displayModules();
 
-        // Find all annotated classes
-        Reflections reflections = new Reflections(packageNameToScan);
-        Set<Class<?>> annotatedClasses = reflections.getTypesAnnotatedWith(Attributes.class);
+        loadDependenciesIntoPluginClasspath();
 
-        getLog().info("Found " + annotatedClasses.size() + " classes with annotation " + Attributes.class.getCanonicalName());
+        System.out.println("-- CLASSES --");
+        System.out.println("Using package: " + packageNameToScan);
+        Set<Class<?>> classes = getClassesFromPackage(packageNameToScan);
 
-        for (Class c: annotatedClasses) {
-
-            JsonSchemaGenerator v4generator = SchemaGeneratorBuilder.draftV4Schema().build();
-            JsonNode productSchema = v4generator.generateSchema(c);
-            System.out.println(productSchema);
-            getLog().info("Generated " + productSchema);
+        if (classes.size() == 0) {
+            getLog().warn("Could not find any classes to generate schemas from.");
+        } else {
+            generateJsonSchemaForClassSet(classes);
         }
     }
+
+    private void generateJsonSchemaForClassSet(Set<Class<?>> classes) {
+
+        JsonSchemaGenerator generator = SchemaGeneratorBuilder.draftV4Schema().build();
+        for (Class c : classes) {
+            ObjectNode node = generator.generateSchema(c);
+            System.out.println("\t Processing: " + c.getName());
+
+            if (DEBUG) {
+                System.out.println(node);
+            }
+
+            try {
+                writeFileToOutput(c, node);
+            } catch (IOException e) {
+                System.err.println("Could not write json schema for " + c.getName() + e);
+            }
+
+        }
+    }
+
+    private void writeFileToOutput(Class c, ObjectNode node) throws IOException {
+
+        File outputDirectory = new File(project.getBasedir() + File.separator + schemaOutputDirectory);
+        if (!outputDirectory.exists()) {
+            outputDirectory.mkdir();
+        }
+
+        String outputFileName = c.getSimpleName() + ".json";
+        File outputFile = new File(outputDirectory + File.separator, outputFileName);
+
+        String formattedJson = JsonWriter.formatJson(node.toString());
+        Files.write(formattedJson, outputFile, Charsets.UTF_8);
+
+        System.out.println("Wrote: " + outputFile.getAbsolutePath());
+    }
+
+    private void loadDependenciesIntoPluginClasspath() {
+
+        try {
+
+            List<String> classElements = project.getCompileClasspathElements();
+
+            ClassRealm realm = descriptor.getClassRealm();
+            for (String element : classElements) {
+                File elementFile = new File(element);
+                realm.addURL(elementFile.toURI().toURL());
+                System.out.println("Added " + elementFile.toURI().toURL().toString());
+            }
+        } catch (DependencyResolutionRequiredException e) {
+            System.out.println("Could not resolve dependency " + e);
+        } catch (MalformedURLException e) {
+            System.out.println("MalformedURLException " + e);
+        }
+    }
+
+    private void displayModules() {
+
+        System.out.println("-- MODULES --");
+        List<String> modules = project.getModules();
+        for (String mod: modules) {
+            System.out.println(mod);
+        }
+    }
+
+    public static Set<Class<?>> getClassesFromPackage(String packageName) {
+
+        Reflections ref = new Reflections(packageName);
+        return ref.getTypesAnnotatedWith(Attributes.class);
+    }
+
 }
